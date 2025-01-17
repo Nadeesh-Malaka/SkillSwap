0b669766371040e5259c0adf53b61ab4d36d5095
Nadeesh Malaka Chathuranga <Nadeeshmalaka2001@gmail.com>
2025-01-17 23:34:18 +0530

    Refactor footer component with updated structure and styling for improved layout and responsiveness

--- skillswap/src/Components/NavFooter/footer.js
+++ skillswap/src/Components/NavFooter/footer.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
 
-function footer() {
+function Footer() {
   return (
-    <div>
-    <footer>
-    <div class="footer-content">
-      <p>© 2024 SkillSwap. All rights reserved. | <a href="#">Terms of Use</a> | <a href="#">Privacy Policy</a></p>
-      <div class="social-links">
-        <a href="#">Facebook</a>
-        <a href="#">Twitter</a>
-        <a href="#">Instagram</a>
+    <footer className="footer">
+      <div className="footer-container">
+        <h2 className="footer-logo">SkillSwap</h2>
+        <ul className="footer-menu">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#blog">Blog</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <div className="social-links">
+          <a href="#" className="social-icon facebook"><i className="fab fa-facebook-f"></i></a>
+          <a href="#" className="social-icon twitter"><i className="fab fa-twitter"></i></a>
+          <a href="#" className="social-icon instagram"><i className="fab fa-instagram"></i></a>
+        </div>
+        <p className="footer-text">© 2024 SkillSwap. All rights reserved.</p>
       </div>
-    </div>
-  </footer>
-
-    </div>
-  )
+    </footer>
+  );
 }
 
-export default footer
+export default Footer;
